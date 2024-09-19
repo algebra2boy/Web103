4094f33ec54be86384a9d975f794b4a11d7e044e
Yongye Tan <yongyetan1209@gmail.com>
2024-09-19 18:27:29 -0400

    Add first project

---
+++ Projects/HallowKnight/server/routes/gifts.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import giftData from '../data/gifts.js';
+
+// The fileURLToPath function is a utility function that converts a file URL to a file path. It takes a single argument, 
+// which is a file URL in the form of a string, and returns a file path in the form of a string. 
+// This function is useful when working with file URLs in Node.js, as many Node.js APIs expect file paths rather than file URLs.
+const __filename = fileURLToPath(import.meta.url); // get the file path
+const __dirname = path.dirname(__filename); // get the directory name of the current module file
+
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+    res.status(200).json(giftData);
+});
+
+router.get('/:giftId', (_req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'));
+});
+
+export default router;
---
+++ Projects/HallowKnight/server/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.use('/public', express.static('./public'));
+app.use('/scripts', express.static('./public/scripts'));
+
+import giftsRouter from './routes/gifts.js';
+
+app.use('/gifts', giftsRouter);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server listening on http://localhost:${PORT}`)
+});
