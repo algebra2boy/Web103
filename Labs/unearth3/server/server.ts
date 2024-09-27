3647d2909b4845979f8e79f91ea713108cbdf19f
Yongye Tan <yongyetan1209@gmail.com>
2024-09-27 19:34:53 -0400

    project 3 started - remove static file

---
+++ Labs/unearth3/server/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import "./config/dotenv.js";
+import giftsRouter from './routes/gifts.js';
+ 
+const app = express();
+
+app.use(cors());
+
+app.use('/gifts', giftsRouter);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server listening on http://localhost:${PORT}`)
+});
