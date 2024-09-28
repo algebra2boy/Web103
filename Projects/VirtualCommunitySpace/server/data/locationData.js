c34f858b0e2b0737a42ef98a6179810aee63e68a
Yongye Tan <yongyetan1209@gmail.com>
2024-09-28 00:56:24 -0400

    add mock data

---
+++ Projects/VirtualCommunitySpace/server/data/locationData.js
@@ -0,0 +1,38 @@
+const locations = [
+    {
+        id: 1,
+        name: "The Echo Lounge & Music Hall",
+        image: "https://assets.livenationcdn.com/uploads/the-echo_pr.jpg",
+        address: "1323 NStemmon Fwy, Dallas, TX 75207",
+        city: "Dallas",
+        state: "TX",
+        zip: "75207",
+    },
+    {
+        id: 2,
+        name: "House of Blues",
+        image: "https://mavenprodcontent.blob.core.windows.net/media/houseofblues_dal/FormsHeaders/Venue_1000x650.png",
+        address: "2200 N Lamar St",
+        city: "Dallas",
+        state: "TX",
+        zip: "75202",
+    },
+    {
+        id: 3,
+        name: "The Pavilion at Toyota Music Factory",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-dzbIu-BVr1A_PZpI5GN7WLzN--GuhaYDyQ&s",
+        address: "300 W Las Colinas Blvd",
+        city: "Irving",
+        state: "TX",
+        zip: "75039"
+    },
+    {
+        id: 4,
+        name: "American airline center",
+        image: "https://upload.wikimedia.org/wikipedia/commons/3/3d/American_Airlines_Center_%286246886325%29_cropped.jpg",
+        address: "2500 Victory Ave",
+        city: "Dallas",
+        state: "TX",
+        zip: "75219",
+    }
+];
