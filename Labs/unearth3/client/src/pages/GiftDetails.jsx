af0c80c32e674889b7005cd3ed709a10e2479ead
Yongye Tan <yongyetan1209@gmail.com>
2024-09-27 20:03:34 -0400

    update frontend fetch call

--- Labs/unearth3/client/src/pages/GiftDetails.jsx
+++ Labs/unearth3/client/src/pages/GiftDetails.jsx
@@ -1,25 +1,31 @@
 
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'
 import './GiftDetails.css'
 
-const GiftDetails = ({data}) => {
+const GiftDetails = ({ data }) => {
 
-    const [gift, setGift] = useState({id: 0, name: "", pricepoint: "", audience: "", image: "", description: "", submittedby: "", submittedon: ""})
+    const { id } = useParams()
 
+    const [gift, setGift] = useState({ id: 0, name: "", pricepoint: "", audience: "", image: "", description: "", submittedby: "", submittedon: "" })
 
     useEffect(() => {
-
-    }, []);
-
+        const fetchGiftById = async () => {
+            const response = await fetch(`http://localhost:3001/gifts/${id}`)
+            const data = await response.json();
+            setGift(data);
+        };
+        fetchGiftById();
+    }, [data, id]);
 
     return (
         <div className="GiftDetails">
-            <main id="gift-content" class="gift-info">
-                <div class="image-container">
+            <main id="gift-content" className="gift-info">
+                <div className="image-container">
                     <img id="image" src={gift.image} />
                 </div>
-                <div class="gift-details">
+                <div className="gift-details">
                     <h2 id="name">{gift.name}</h2>
                     <p id="submittedBy">{'Submitted By: ' + gift.submittedby}</p>
                     <p id="pricePoint">{'Price: ' + gift.pricepoint}</p>
