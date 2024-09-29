2467f4e268aa2327dbf4549238669265bf81c263
Yongye Tan <yongyetan1209@gmail.com>
2024-09-29 13:26:47 -0400

    finished lab 4

--- Labs/unearth4/server/server.ts
+++ Labs/unearth4/server/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import "./config/dotenv.js";
+import morgan from 'morgan';
 import giftsRouter from './routes/gifts.js';
  
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/gifts', giftsRouter);
 
