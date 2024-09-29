7e7c0a19792f98210567180bf28dfa0eddf0ffc9
Yongye Tan <yongyetan1209@gmail.com>
2024-09-28 20:55:32 -0400

    add logic for get event by location ID

--- Projects/VirtualCommunitySpace/server/controllers/eventController.js
+++ Projects/VirtualCommunitySpace/server/controllers/eventController.js
@@ -2,7 +2,21 @@ import { pool } from "../config/database.js";
 
 const getAllEvents = async (req, res) => {
     try {
-        const results = await pool.query('SELECT * FROM events ORDER BY id ASC'); 
+        const results = await pool.query('SELECT * FROM events ORDER BY id ASC');
+        res.status(200).json(results.rows);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const getsEventByLocationID = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const results = await pool.query(`
+            SELECT * FROM events
+            WHERE locationid = $1 
+            ORDER BY id ASC
+        `, [id]);
         res.status(200).json(results.rows);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -11,5 +25,6 @@ const getAllEvents = async (req, res) => {
 
 
 export default {
-    getAllEvents
+    getAllEvents,
+    getsEventByLocationID
 }
--- Projects/VirtualCommunitySpace/server/routes/event.js
+++ Projects/VirtualCommunitySpace/server/routes/event.js
@@ -4,5 +4,6 @@ import EventController from '../controllers/eventController.js';
 const router = express.Router();
 
 router.get('/event', EventController.getAllEvents);
+router.get('/event/:id', EventController.getsEventByLocationID);
 
 export default router;
