1b2b5778d298effaec1b48cdff0c6e318a897eb0
Yongye Tan <yongyetan1209@gmail.com>
2024-09-28 20:38:44 -0400

    add database to backend

--- Projects/VirtualCommunitySpace/server/config/reset.js
+++ Projects/VirtualCommunitySpace/server/config/reset.js
@@ -1,8 +1,12 @@
 import { pool } from './database.js';
+import './dotenv.js';
 import { locations } from '../data/locationData.js';
+import { events } from '../data/eventsData.js';
 
+// Start of the location query
+// CASCADE is used to drop all dependent objects in the table
 const createLocationQuery = `
-    DROP TABLE IF EXISTS locations;
+    DROP TABLE IF EXISTS locations CASCADE;
 
     CREATE TABLE IF NOT EXISTS locations (
         id SERIAL PRIMARY KEY,
@@ -11,7 +15,7 @@ const createLocationQuery = `
         address VARCHAR(255) NOT NULL,
         city VARCHAR(255) NOT NULL,
         state VARCHAR(255) NOT NULL,
-        zip VARCHAR(255) NOT NULL,
+        zip VARCHAR(255) NOT NULL
     )
 `
 
@@ -49,5 +53,60 @@ const seedLocationsTable = async () => {
         });
     });
 }
+// End of the location query
 
-seedLocationsTable();
+const createEventQuery = `
+    DROP TABLE IF EXISTS events;
+
+    CREATE TABLE IF NOT EXISTS events (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        date VARCHAR(255) NOT NULL,
+        time INT NOT NULL,
+        image VarChar(255) NOT NULL,
+        locationId INT NOT NULL,
+        FOREIGN KEY (locationId) references locations(id)
+    )
+`
+
+const createEventTable = async () => {
+    try {
+        const res = await pool.query(createEventQuery);
+    } catch (err) {
+        console.error('⚠️ error creating events table', err);
+    }
+};
+
+const seedEventTable = async () => {
+    await createEventTable();
+
+    events.forEach((event) => {
+        const insertQuery = {
+            text: 'INSERT INTO events (title, date, time, image, locationId) VALUES ($1, $2, $3, $4, $5)',
+        }
+
+        const values = [
+            event.title,
+            event.date,
+            event.time,
+            event.image,
+            event.locationId
+        ];
+
+        pool.query(insertQuery.text, values, (err, res) => {
+            if (err) {
+                console.error('⚠️ error inserting event', err);
+            } else {
+                console.log('🎉 event inserted successfully');
+            }
+        });
+    })
+}
+
+const seedDatabase = async () => {
+    await seedLocationsTable();
+    await seedEventTable();
+    console.log('🌱 seeding locations and events tables...');
+}
+
+seedDatabase();
--- Projects/VirtualCommunitySpace/server/data/eventsData.js
+++ Projects/VirtualCommunitySpace/server/data/eventsData.js
@@ -1,11 +1,11 @@
-const events = [
+export const events = [
     {
         "id": 1,
         "title": "Wage War - The Manic Tour",
         "date": "Apr 14, 2023",
         "time": 1800,
         "image": "https://i.iheart.com/v3/catalog/artist/30912460?ops=fit(720%2C720)",
-        "location": 1
+        "locationId": 1
     },
     {
         "id": 2,
@@ -13,7 +13,7 @@ const events = [
         "date": "Apr 21, 2023",
         "time": 1800,
         "image": "https://i.scdn.co/image/ab6761610000e5eb9e6c14bd347a74387a544d05",
-        "location": 1
+        "locationId": 1
     },
     {
         "id": 3,
@@ -21,7 +21,7 @@ const events = [
         "date": "Mar 23, 2023",
         "time": 1900,
         "image": "https://bandwagmag.com/wp-content/uploads/2016/12/10-og.jpg",
-        "location": 2
+        "locationId": 2
     },
     {
         "id": 4,
@@ -29,7 +29,7 @@ const events = [
         "date": "May 14, 2023",
         "time": 1900,
         "image": "https://i.scdn.co/image/ab6761610000e5ebe63ceef20dce0ac6522c4011",
-        "location": 2
+        "locationId": 2
     },
     {
         "id": 5,
@@ -37,7 +37,7 @@ const events = [
         "date": "May 08, 2023",
         "time": 2000,
         "image": "https://static01.nyt.com/images/2013/03/31/magazine/31kareno4/31kareno4-superJumbo-v3.jpg",
-        "location": 3
+        "locationId": 3
     },
     {
         "id": 6,
@@ -45,7 +45,7 @@ const events = [
         "date": "Jun 05, 2023",
         "time": 1900,
         "image": "https://static.stereogum.com/uploads/2021/05/The-Green-Album-1620834349-scaled.jpeg",
-        "location": 3
+        "locationId": 3
     },
     {
         "id": 7,
@@ -53,7 +53,7 @@ const events = [
         "date": "Jul 22, 2023",
         "time": 1700,
         "image": "https://merchbar.imgix.net/product/105/6519/4001755627602/ODAyiKAK85-1.png?quality=60&auto=compress,format&w=3840",
-        "location": 3
+        "locationId": 3
     },
     {
         "id": 8,
@@ -61,7 +61,7 @@ const events = [
         "date": "Jul 24, 2023",
         "time": 1400,
         "image": "https://assets.website-files.com/609075d36493a9d63653ead7/63c74cf342c0edb9de0a2826_SSF23_Website_MobileHeader.png",
-        "location": 3
+        "locationId": 3
     },
     {
         "id": 9,
@@ -69,7 +69,7 @@ const events = [
         "date": "Jul 26, 2023",
         "time": 1900,
         "image": "https://i.scdn.co/image/ab6761610000e5eb952381a70ceeda02554ad545",
-        "location": 3
+        "locationId": 3
     },
 
     {
@@ -78,7 +78,7 @@ const events = [
         "date": "Sep 21, 2023",
         "time": 2000,
         "image": "https://iscale.iheart.com/catalog/artist/36646",
-        "location": 3
+        "locationId": 3
     },
 
     {
@@ -87,7 +87,7 @@ const events = [
         "date": "May 12, 2023",
         "time": 1800,
         "image": "https://i.scdn.co/image/ab6761610000e5eb95414338c072c7655e751bf1",
-        "location": 1
+        "locationId": 1
     },
 
     {
@@ -96,7 +96,7 @@ const events = [
         "date": "May 21, 2023",
         "time": 1900,
         "image": "https://i.scdn.co/image/ab6761610000e5eb6a29072c320c99c245fe24e2",
-        "location": 1
+        "locationId": 1
     },
 
     {
@@ -105,7 +105,7 @@ const events = [
         "date": "Jun 10, 2023",
         "time": 1900,
         "image": "https://static.stereogum.com/uploads/2015/09/duranduran.jpg",
-        "location": 4
+        "locationId": 4
     },
 
     {
@@ -114,7 +114,7 @@ const events = [
         "date": "Sep 18, 2023",
         "time": 2030,
         "image": "https://yt3.googleusercontent.com/T8Up4YxiN4wGfkyI6J8OyRbQNWiXWv8Lb-I-CjxE9Hi7DyFbcIShXW3i2TqqmXtECcaqlDXk1A=s900-c-k-c0x00ffffff-no-rj",
-        "location": 4
+        "locationId": 4
     },
 
     {
@@ -123,6 +123,6 @@ const events = [
         "date": "Oct 01, 2023",
         "time": 1900,
         "image": "https://i.discogs.com/tiuVvZ5R0TWQDXYr4LabpmOOY_l123e7FoXOXk4y3ck/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTI3MjUt/MTY2NTMzMzEyMS0z/OTk4LmpwZWc.jpeg",
-        "location": 4
+        "locationId": 4
     }
 ]
